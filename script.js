1373f812b632ce975f50c3a2109bc485d3370aba
unknown <akashkumar200124@gmail.com>
2024-04-26 01:20:18 +0530

    initial changes road map

---
+++ script.js
@@ -0,0 +1,62 @@
+
+// showAlert()
+// {
+//     alert("this is something");
+// }
+
+
+function internet() {
+
+    alert("Hii internet");
+};
+
+function html() {
+
+    alert("Hii html");
+};
+
+function css() {
+
+    alert("Hii css");
+};
+
+function js() {
+
+    alert("Hii js");
+};
+
+function vcs() {
+
+    alert("Hii vcs");
+};
+
+function vscHosting() {
+
+    alert("Hii vscHosting");
+};
+
+function pmanager() {
+
+    alert("Hii package manager");
+};
+
+function frame() {
+
+    alert("Hii frame");
+};
+
+function hiicss() {
+
+    alert("Hii css frame");
+};
+
+function css_archi() {
+
+    alert("Hii css archi");
+};
+
+function css_pros() {
+
+    alert("Hii css pros");
+};
+
